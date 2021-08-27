5c6dc571d2fddcf66154a09613c2704f2daeefd0
HansolKo94 <lavalliere125@gmail.com>
2021-08-27 19:23:30 +0900

    [추가] 패키지구조

---
+++ Yeokku/src/main/java/com/kh/yeokku/model/biz/impl/AdminpageBizImpl.java
@@ -0,0 +1,16 @@
+package com.kh.yeokku.model.biz.impl;
+
+import java.util.List;
+
+import com.kh.yeokku.model.biz.AdminpageBiz;
+import com.kh.yeokku.model.dto.UserDto;
+
+public class AdminpageBizImpl implements AdminpageBiz{
+
+	@Override
+	public List<UserDto> select_user_list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Yeokku/src/main/java/com/kh/yeokku/model/dao/impl/AdminpageDaoImpl.java
@@ -0,0 +1,16 @@
+package com.kh.yeokku.model.dao.impl;
+
+import java.util.List;
+
+import com.kh.yeokku.model.dao.AdminpageDao;
+import com.kh.yeokku.model.dto.UserDto;
+
+public class AdminpageDaoImpl implements AdminpageDao {
+
+	@Override
+	public List<UserDto> select_user_list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
